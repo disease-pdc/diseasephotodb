730e5cf0142be3040fab020621e8ec194404256d
Adam Wilson <adam@bastion.dev>
2022-04-03 16:18:52 -0400

    Added redis initializer

---
+++ config/initializers/redis.rb
@@ -0,0 +1,8 @@
+Rails.application.config.tfimagerydb_redis = {
+  url: ENV['REDIS_URL'].present? ? ENV[ENV['REDIS_URL']] || ENV['REDIS_URL'] : nil,
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+}
+
+REDIS_POOL = ConnectionPool.new(size: 5) do
+  Redis.new Rails.application.config.tfimagerydb_redis
+end
