b2800bef34f184dae3c7a9c00203f29268863d09
Adam Wilson <adam@bastion.dev>
2022-05-17 16:34:39 -0400

    Added correct user grading set complete count

--- app/models/grading_set.rb
+++ app/models/grading_set.rb
@@ -12,7 +12,20 @@ class GradingSet < ApplicationRecord
   end
 
   def user_grading_sets_complete_count
-    0
+    ActiveRecord::Base.connection.execute(%(
+      with grading_set_images_count as (
+        select count(*) as count from grading_set_images where grading_set_id = #{id}
+      ), complete_users as (
+        select user_grading_set_images.user_id as id, count(user_grading_set_images.id) as count
+        from user_grading_set_images
+        join grading_set_images on grading_set_images.id = user_grading_set_images.grading_set_image_id
+        where grading_set_images.grading_set_id = #{id}
+        group by user_grading_set_images.user_id
+      )
+      select count(complete_users.id) as count
+      from complete_users, grading_set_images_count
+      where complete_users.count = grading_set_images_count.count
+    )).first['count']
   end
 
   def data_to_csv
