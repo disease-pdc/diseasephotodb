9246610e1a48323b217525cbf205eb76ead61467
Adam Wilson <adam@bastion.dev>
2022-06-17 09:15:46 -0400

    Use proper remote URL for images in worker

--- app/jobs/ExifJob.rb
+++ app/jobs/ExifJob.rb
@@ -8,21 +8,21 @@ class ExifJob
   def perform id
     image = Image.find id
     if image.filename =~ /.jp/i
-      image.exif_data = EXIFR::JPEG.new(open(get_image_url(image))).to_hash
+      image.exif_data = EXIFR::JPEG.new(get_image(image)).to_hash
       image.save!
     elsif image.filename =~ /.tif/i
-      image.exif_data = EXIFR::TIFF.new(open(get_image_url(image))).to_hash
+      image.exif_data = EXIFR::TIFF.new(get_image(image)).to_hash
       image.save!
     end
   end
 
   private
 
-    def get_image_url image
+    def get_image image
       if ActiveStorage::Blob.service.name == :local
-        ActiveStorage::Blob.service.path_for image.image_file.key
+        open(ActiveStorage::Blob.service.path_for image.image_file.key)
       else
-        image.image_file.url
+        URI.open(image.image_file.url)
       end
     end
 
