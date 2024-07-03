a6a316ea8e6d40426bd4b5d7c4efb6e626a356b7
Adam Wilson <adam@bastion.dev>
2024-07-03 11:24:58 -0400

    Batch image downloading

--- app/javascript/packs/images_download.jsx
+++ app/javascript/packs/images_download.jsx
@@ -18,12 +18,24 @@ const ImagesDownload = ({
   imageSourceId
 }) => {
 
+  const batchSize = 200;
+
   const [sourceId, setSourceId] = useState(imageSourceId)
-  const [imageData, setImageData] = useState([]) 
+  const [imageData, setImageData] = useState(null) 
 
   const [downloading, setDownloading] = useState(false)
   const [downloadingPercent, setDownloadingPercent] = useState(0.0)
   const [downloadingFile, setDownloadingFile] = useState(null)
+
+
+  useEffect(() => {
+    if (sourceId) {
+      async function fetchData() {
+        setImageData(await getImageData({ imageSourceId: sourceId }))
+      }
+      fetchData();
+    }
+  }, [sourceId])
 
   const getSourceName = (sourceId) => {
     for (let i = 0; i < imageSources.length; i++) {
@@ -33,10 +45,10 @@ const ImagesDownload = ({
     }
   }
 
-  const doDownload = async () => {
+  const doDownload = async (batchIndex) => {
     setDownloading(true)
     setDownloadingPercent(0)
-    const images = await getImageData({ imageSourceId: sourceId })
+    const images = imageData.slice(batchIndex * batchSize, (batchIndex + 1) * batchSize);
 
     let imageBlobs = []
     for (let i = 0; i < images.length; i++) {
@@ -62,7 +74,7 @@ const ImagesDownload = ({
     ).then(zipFile => {
       const currentDate = new Date().getTime()
       const sourceName = getSourceName(sourceId) || 'images_sources'
-      const fileName = `${sourceName}.zip`
+      const fileName = `${sourceName}.${batchIndex + 1}.zip`
       setDownloading(false)
       setDownloading(false)
       return FileSaver.saveAs(zipFile, fileName);
@@ -71,66 +83,80 @@ const ImagesDownload = ({
 
   return (
     <>
-      {!downloading && 
-        <>
-          <div className="row">
-            <div className="col-lg-6">
-              <label htmlFor="metadataFile" className="form-label">
-                Select Image Folder
-              </label>
-              <select className="form-select mb-3"
-                value={sourceId}
-                onChange={e => setSourceId(e.target.value)}
-              >
-                <option value="-1"></option>
-                {imageSources.map(({id,name}) => (
-                  <option key={id} value={id}>{name}</option>
-                ))}
-              </select>
-            </div>
-            <div className="col-lg-6">
-              <label className="form-label">
-                &nbsp;
-              </label>
-              <button disabled={!sourceId || sourceId === "" || sourceId === "-1"}
-                className="btn btn-primary d-block" 
-                type="button"
-                onClick={doDownload}
-              >
-                Download images
-              </button>
-            </div>
-          </div>
-        </>
-      }
-      {downloading &&
-        <>
-          <div className="progress">
-            <div className="progress-bar progress-bar-striped progress-bar-animated" 
-              role="progressbar" 
-              aria-valuenow={downloadingPercent} 
-              aria-valuemin="0" 
-              aria-valuemax="100"
-              style={{width: `${downloadingPercent}%`}}
+      <>
+        <div className="row">
+          <div className="col-lg-6">
+            <label htmlFor="metadataFile" className="form-label">
+              Select Image Folder
+            </label>
+            <select className="form-select mb-3"
+              value={sourceId}
+              onChange={e => setSourceId(e.target.value)}
             >
-            </div>
+              <option value="-1"></option>
+              {imageSources.map(({id,name}) => (
+                <option key={id} value={id}>{name}</option>
+              ))}
+            </select>
           </div>
-          {downloadingPercent <= 0.0 &&
-            <p>Fetching image list from database.</p>
-          }
-          {downloadingPercent > 0.0 && downloadingPercent < 100.0 &&
-            <>
-              <p>Downloading and zipping images.</p>
-              {downloadingFile &&
-                  <pre>{downloadingFile}</pre>
-              }
-            </>
-          }
-        </>
-      }
-      {downloadingPercent >= 100.0 &&
-        <p><hr/>Image downloading complete, your file should be saved shortly.</p>
-      }
+        </div>
+        {downloading &&
+          <>
+            <hr/>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped progress-bar-animated" 
+                role="progressbar" 
+                aria-valuenow={downloadingPercent} 
+                aria-valuemin="0" 
+                aria-valuemax="100"
+                style={{width: `${downloadingPercent}%`}}
+              >
+              </div>
+            </div>
+            {downloadingPercent <= 0.0 &&
+              <p>Fetching image list from database.</p>
+            }
+            {downloadingPercent > 0.0 && downloadingPercent < 100.0 &&
+              <>
+                <p>Downloading and zipping images.</p>
+                {downloadingFile &&
+                    <pre>{downloadingFile}</pre>
+                }
+              </>
+            }
+          </>
+        }
+        {downloadingPercent >= 100.0 &&
+          <>
+            <hr/>
+            <p>Image downloading complete, your file should be saved shortly.</p>
+          </>
+        }
+        {imageData &&
+          <>
+            <hr/>
+            <h5>Image Count: {imageData.length}</h5>
+            {Array.from({length: Math.ceil(imageData.length / batchSize)}, (x, i) => (
+              <div className="row mb-3 align-items-center" key={i}>
+                <div className="col-auto text-right">
+                  <label className="col-form-label">
+                    Download images {i * batchSize + 1} through {(i + 1) * batchSize > imageData.length ? imageData.length : (i + 1) * batchSize }:
+                  </label>
+                </div>
+                <div className="col-auto">
+                  <button disabled={downloading}
+                    className="btn btn-primary btn-sm d-block" 
+                    type="button"
+                    onClick={() => doDownload(i)}
+                  >
+                    Download
+                  </button>
+                </div>
+              </div>
+            ))}
+          </>
+        }
+      </>
     </>
   )
 }
